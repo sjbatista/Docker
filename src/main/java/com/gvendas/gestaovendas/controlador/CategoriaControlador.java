6b41129eede314a55eff781331d42aa053e37ab3
sjbatista <salomaojunio@hotmail.com>
2022-02-15 15:24:33 -0300

    new methods and services

--- src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
+++ src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +31,14 @@ public class CategoriaControlador {
 	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo){
-		Optional<Categoria> categoria = categoriaServico.buscarPorId(codigo);
+		Optional<Categoria> categoria = categoriaServico.buscarPorCodigo(codigo);
 		return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();  
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria){
+		Categoria categoriaSalva = categoriaServico.salvar(categoria);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 }
 
--- src/main/java/com/gvendas/gestaovendas/servico/CategoriaServico.java
+++ src/main/java/com/gvendas/gestaovendas/servico/CategoriaServico.java
@@ -3,7 +3,9 @@ package com.gvendas.gestaovendas.servico;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gvendas.gestaovendas.repositorio.CategoriaRepositorio;
@@ -19,8 +21,27 @@ public class CategoriaServico {
 		return categoriaRepositorio.findAll();
 	}
 	
-	public Optional<Categoria> buscarPorId(Long codigo){
+	public Optional<Categoria> buscarPorCodigo(Long codigo){
 		return categoriaRepositorio.findById(codigo);
+	}
+	
+	public Categoria salvar(Categoria categoria) {
+		return categoriaRepositorio.save(categoria);
+	}
+	
+	public Categoria atualizar(Long codigo, Categoria categoria) {
+		Categoria categoriaSalvar = validarCategoriaExiste(codigo);
+		BeanUtils.copyProperties(categoria, categoriaSalvar, "codigo");
+		return categoriaRepositorio.save(categoriaSalvar);
+		
+	}
+
+	private Categoria validarCategoriaExiste(Long codigo) {
+		Optional<Categoria> categoria = buscarPorCodigo(codigo);
+		if(categoria.isEmpty()) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		return categoria.get();
 	}
 	
 }

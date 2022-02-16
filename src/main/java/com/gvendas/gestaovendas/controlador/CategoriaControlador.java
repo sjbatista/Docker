af60cbe1ee1876e5b70de12f15b1abcb7309c833
sjbatista <salomaojunio@hotmail.com>
2022-02-16 11:52:43 -0300

    new method in Controller

--- src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
+++ src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,11 @@ public class CategoriaControlador {
 	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria){
 		Categoria categoriaSalva = categoriaServico.salvar(categoria);
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @RequestBody Categoria categoria){
+		return ResponseEntity.ok(categoriaServico.atualizar(codigo, categoria));
 	}
 }
 

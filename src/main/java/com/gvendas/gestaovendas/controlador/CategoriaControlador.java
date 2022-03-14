ea086560fb2805f8028b3444d4d8add8edf3b6a9
sjbatista <salomaojunio@hotmail.com>
2022-03-14 13:08:47 -0300

    continuation

--- src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
+++ src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
@@ -19,6 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gvendas.gestaovendas.entidades.Categoria;
 import com.gvendas.gestaovendas.servico.CategoriaServico;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("categoria")
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaControlador {
@@ -26,24 +30,27 @@ public class CategoriaControlador {
 	@Autowired
 	private CategoriaServico categoriaServico;
 	
+	@ApiOperation(value = "listar")
 	@GetMapping
 	public List<Categoria> listarTodas(){
 		return categoriaServico.listarTodas();
 	}
 	
-	
+	@ApiOperation(value = "listar por código")
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo){
 		Optional<Categoria> categoria = categoriaServico.buscarPorCodigo(codigo);
 		return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();  
 	}
 	
+	@ApiOperation(value = "salvar")
 	@PostMapping
 	public ResponseEntity<Categoria> salvar(@Valid @RequestBody Categoria categoria){
 		Categoria categoriaSalva = categoriaServico.salvar(categoria);
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
+	@ApiOperation(value = "atualizar")
 	@PutMapping("/{codigo}")
 	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo,@Valid @RequestBody Categoria categoria){
 		return ResponseEntity.ok(categoriaServico.atualizar(codigo, categoria));

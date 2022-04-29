93a166b4f80b248f894bd3670204ff2ef468d5c9
sjbatista <salomaojunio@hotmail.com>
2022-04-29 13:28:05 -0300

    new commit

--- src/main/java/com/gvendas/gestaovendas/servico/CategoriaServico.java
+++ src/main/java/com/gvendas/gestaovendas/servico/CategoriaServico.java
@@ -7,7 +7,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-
 import com.gvendas.gestaovendas.repositorio.CategoriaRepositorio;
 import com.gvendas.gestaovendas.entidades.Categoria;
 
